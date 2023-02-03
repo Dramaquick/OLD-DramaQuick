2c07b9147994826dfc383f015af716f4f30f248e
StellarFX <priveeofficiel@gmail.com>
2023-02-03 16:34:00 +0100

    💄 Added focus style on TextBox component

--- resources/js/Components/TextBox.svelte
+++ resources/js/Components/TextBox.svelte
@@ -1,4 +1,5 @@
 <script>
+    import clickOutside from './util/clickOutside.js';
     let className = "";
 
     export { className as class };
@@ -11,11 +12,20 @@
     export let pattern = "";
     export let minlength = "";
     export let maxlength = "";
+    export let showIcon = true;
+
+    let focused = false;
 </script>
 
 
-<div class="text-input">
-    <img src="/img/components/tirets.svg" alt="Tirets">
+<div class={`text-input ${focused ? "focused" : ''}`} use:clickOutside on:outclick={() => focused = false} on:click={() => focused = true}>
+    {#if showIcon}
+        <svg width="14" height="14" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.89575 3.5C1.89575 3.25838 2.09163 3.0625 2.33325 3.0625H11.6666C11.9082 3.0625 12.1041 3.25838 12.1041 3.5C12.1041 3.74162 11.9082 3.9375 11.6666 3.9375H2.33325C2.09163 3.9375 1.89575 3.74162 1.89575 3.5Z" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.89575 7C1.89575 6.75838 2.09163 6.5625 2.33325 6.5625H11.6666C11.9082 6.5625 12.1041 6.75838 12.1041 7C12.1041 7.24162 11.9082 7.4375 11.6666 7.4375H2.33325C2.09163 7.4375 1.89575 7.24162 1.89575 7Z"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.89575 10.5C1.89575 10.2584 2.09163 10.0625 2.33325 10.0625H9.33325C9.57488 10.0625 9.77075 10.2584 9.77075 10.5C9.77075 10.7416 9.57488 10.9375 9.33325 10.9375H2.33325C2.09163 10.9375 1.89575 10.7416 1.89575 10.5Z"/>
+        </svg>
+    {/if}
     <input type="text"
     class="{className}"
     {...$$restProps}
@@ -34,8 +44,11 @@
 <style>
 
 
-    img {
+    svg {
         width: 1.7rem;
+        transform: scale(1.8);
+        fill: #BABABA;
+        transition: all 0.2s ease;
     }
 
     .text-input {
@@ -53,6 +66,7 @@
         display: flex;
         align-items: center;
         justify-content: space-between;
+        transition: all 0.2s ease;
 
     }
 
@@ -69,6 +83,15 @@
 
         color: #BABABA;
 
+    }
+
+    .text-input.focused {
+        border-color: #00E589;
+        box-shadow: 0 0 2px #00E589;
+    }
+
+    .text-input.focused svg {
+        fill: #00E589;
     }
 
 </style>
