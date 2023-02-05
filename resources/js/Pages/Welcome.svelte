4cf06abe6aee4d6871f7608d7342dc7c39dc0b2c
StellarFX <priveeofficiel@gmail.com>
2023-02-05 12:52:39 +0100

    🎨 Reformatted Welcome page

--- resources/js/Pages/Welcome.svelte
+++ resources/js/Pages/Welcome.svelte
@@ -1,6 +1,6 @@
 <script>
     import { Link, page } from "@inertiajs/inertia-svelte";
-    import {onMount} from "svelte";
+    import { onMount } from "svelte";
     import Button from "../Components/Button.svelte";
     import RadioButton from "../Components/RadioButton.svelte";
     import Checkbox from "@/Components/Checkbox.svelte";
@@ -20,7 +20,16 @@
     // export let errors;
     // /** @type {{date: Date, email: string, email_verified_at: null, id: int, name: string, updated_at: Date}}*/
     // export let auth;
-    let form = {selected: 1, slidersimple: [5], sliderdouble: [0,5], text: "", textArea: "", checked: false, selectValue: 0, counter: 0};
+    let form = {
+        selected: 1,
+        slidersimple: [5],
+        sliderdouble: [0, 5],
+        text: "",
+        textArea: "",
+        checked: false,
+        selectValue: 0,
+        counter: 0,
+    };
     // Celles-ci sont définies automatiquement par Inertia
     // A savoir que la variable auth est un objet qui permet d'accéder aux attributs publics de l'utilisateur :
 
@@ -55,32 +64,72 @@
         <Button class="outline">Créer une session</Button>
         <Button class="" disabled>Disabled</Button>
         <Button class="outline" disabled>Disabled outline</Button>
-        <RadioButton class="" name="test" value={1} bind:selected={form.selected}></RadioButton>
-        <RadioButton class="" name="test" value={2} bind:selected={form.selected} disabled></RadioButton>
-        <RadioButton class="" name="test" value={3} bind:selected={form.selected}></RadioButton>
-        <RadioButton class="" name="test" value={4} bind:selected={form.selected} disabled></RadioButton>
-        <Slider type="simple" class="" bind:values={form.slidersimple} min={0} max={10}></Slider>
-        <Slider type="double" bind:values={form.sliderdouble} min={0} max={10}></Slider>
-        <Checkbox bind:checked={form.checked}/>
-        <TextBox bind:value={form.text} placeholder="Entrez du texte..."/>
-        <SelectList bind:value={form.selectValue} placeholder="Sélectionnez une valeur..."/>
-        <TextArea bind:value={form.textArea} placeholder="Entrez du texte..."/>
-        <TimeDisplay minutes={minutes} seconds={seconds}/>
-        <Counter max={15} min={-10} bind:value={form.counter}/>
-        <p>{form.selected}</p>
-        <p>{form.slidersimple}</p>
-        <p>{form.sliderdouble}</p>
-        <p>{form.checked}</p>
-        <p>{form.text}</p>
-        <p>{form.selectValue}</p>
-        <p>{form.textArea}</p>
-        <p>{form.counter}</p>
+        <RadioButton
+            class=""
+            name="test"
+            value={1}
+            bind:selected={form.selected}
+        />
+        <RadioButton
+            class=""
+            name="test"
+            value={2}
+            bind:selected={form.selected}
+            disabled
+        />
+        <RadioButton
+            class=""
+            name="test"
+            value={3}
+            bind:selected={form.selected}
+        />
+        <RadioButton
+            class=""
+            name="test"
+            value={4}
+            bind:selected={form.selected}
+            disabled
+        />
+        <Slider
+            type="simple"
+            class=""
+            bind:values={form.slidersimple}
+            min={0}
+            max={10}
+        />
+        <Slider
+            type="double"
+            bind:values={form.sliderdouble}
+            min={0}
+            max={10}
+        />
+        <Checkbox bind:checked={form.checked} />
+        <TextBox bind:value={form.text} placeholder="Entrez du texte..." />
+        <SelectList
+            bind:value={form.selectValue}
+            placeholder="Sélectionnez une valeur..."
+        />
+        <TextArea bind:value={form.textArea} placeholder="Entrez du texte..." />
+        <TimeDisplay {minutes} {seconds} />
+        <Counter max={15} min={-10} bind:value={form.counter} />
+        <div class="flex gap-8">
+            <p>{form.selected}</p>
+            <p>{form.slidersimple}</p>
+            <p>{form.sliderdouble}</p>
+            <p>{form.checked}</p>
+            <p>{form.text}</p>
+            <p>{form.selectValue}</p>
+            <p>{form.textArea}</p>
+            <p>{form.counter}</p>
+        </div>
     </div>
-    <div>
-        <h1>Une nouvelle version de DramaQuiz</h1>
-    </div>
-    <div>
-        <p>DramaQuick est une version de DramaQuiz accélérée ; des questionnaires temporaires, des sessions de quiz... Tout cela a été créé avec une idée : l’éphémérité !</p>
+    <div class="flex flex-col gap-6 w-1/4">
+        <h1 class="font-semibold text-[2rem] text-black">Une nouvelle version de DramaQuiz</h1>
+        <p class="text-lg">
+            DramaQuick est une version de DramaQuiz accélérée ; des
+            questionnaires temporaires, des sessions de quiz... Tout cela a été
+            créé avec une idée : l’éphémérité !
+        </p>
     </div>
 </main>
 
@@ -99,28 +148,5 @@
         right: -8rem;
         overflow: hidden;
         border-radius: 5rem;
-    }
-
-    h1 {
-        position: absolute;
-        width: 37.875rem;
-        height: 8.375rem;
-        font-family: 'Poppins';
-        font-style: normal;
-        font-weight: 600;
-        font-size: 3rem;
-        line-height: 4.5rem;
-        color: #000000;
-    }
-    p {
-        position: absolute;
-        width: 37.875rem;
-        height: 9.125rem;
-        font-family: 'Poppins';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 1.5rem;
-        line-height: 2.25rem;
-        color: #000000;
     }
 </style>
