f859e3e2c614b04e60c438a2389f97e71a75a149
Hugo.M <hugo.rog@icloud.com>
2023-02-08 19:04:24 +0100

    💄 Update <table> to flexbox

--- resources/js/Pages/Welcome.svelte
+++ resources/js/Pages/Welcome.svelte
@@ -4,33 +4,28 @@
 </svelte:head>
 
 <!-- Contenu de la page -->
-<main class="min-h-screen w-full">
+<main class="min-h-screen w-full font-['Poppins']">
     <div class="flex flex-col gap-6 w-1/4">
-        <h1 class="font-semibold text-[2rem] text-black">Une nouvelle version de DramaQuiz</h1>
-        <p class="text-lg">
+        <h1 class="font-w600 text-[3rem] text-black width-606">Une nouvelle version de DramaQuiz</h1>
+        <p class="text-lg font-w400 width-606 text-[1.5rem]">
             DramaQuick est une version de DramaQuiz accélérée ; des
             questionnaires temporaires, des sessions de quiz... Tout cela a été
             créé avec une idée : l’éphémérité !
         </p>
     </div>
-    <div>
-        <table>
-            <tr class="text-[#34FFAD] text-5xl font-['Poppins']" >
-                <th class="pr-16">15</th>
-                <th class="pr-16">3</th>
-                <th class="pr-16">124 </th>
-            </tr>
-            <tr class="text-2xl text-[#273C33] font-['Poppins'] leading-9">
-                <th class="pr-16">Quizz</th>
-                <th class="pr-16">Sessions</th>
-                <th class="pr-16">Participa-</th>
-            </tr>
-            <tr class="text-2xl text-[#273C33] font-['Poppins'] leading-9">
-                <th class="pr-16">réalisés</th>
-                <th class="pr-16">actives</th>
-                <th class="pr-16">tions</th>
-            </tr>
-        </table>
+    <div class="flex flex-row gap-6 text-center">
+        <div>
+            <h2 class="text-[#34FFAD] text-5xl font-w600 margin-r3">15</h2>
+            <p class="text-2xl text-[#273C33] leading-9 font-w400 margin-r3">Quizz<br>réalisés</p>
+        </div>
+        <div>
+            <h2 class="text-[#34FFAD] text-5xl font-w600 margin-r3">3</h2>
+            <p class="text-2xl text-[#273C33] leading-9 font-w400 margin-r3">Sessions<br>actives</p>
+        </div>
+        <div>
+            <h2 class="text-[#34FFAD] text-5xl font-w600">124</h2>
+            <p class="text-2xl text-[#273C33] leading-9 font-w400">Participa-<br>tions</p>
+        </div>
     </div>
 </main>
 
@@ -49,5 +44,29 @@
         right: -8rem;
         overflow: hidden;
         border-radius: 5rem;
+
+    }
+
+    .font-w600 {
+
+        font-weight: 600;
+
+    }
+
+    .font-w400 {
+
+        font-weight: 400;
+
+    }
+
+    .width-606 {
+
+        width: 606px;
+
+    }
+
+    .margin-r3 {
+
+            margin-right: 3rem;
     }
 </style>
