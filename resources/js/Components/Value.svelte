a87931cb562a321921e309c3622bec2f43ce4950
Hugo.M <hugo.rog@icloud.com>
2023-02-14 19:32:21 +0100

    ➡️ Added Value components

---
+++ resources/js/Components/Value.svelte
@@ -0,0 +1,23 @@
+<script>
+    let className = "";
+
+    export { className as class };
+    export let value = "";
+</script>
+
+<h2 class="" {value}>
+    {value}
+</h2>
+
+<style>
+    h2 {
+        display: flex;
+        color: #34FFAD;
+        font-size: 3rem;
+        font-weight: 600;
+        text-align: center;
+        align-items: center;
+        height: 3rem;
+        justify-content: center;
+    }
+</style>
