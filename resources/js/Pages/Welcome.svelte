fed0af9c7c462aaa1f70f4536042350e7acaa328
Hugo.M <hugo.rog@icloud.com>
2023-02-14 19:31:38 +0100

    💄 Added style and images on Welcome

--- resources/js/Pages/Welcome.svelte
+++ resources/js/Pages/Welcome.svelte
@@ -1,5 +1,7 @@
 <script>
-    import Button from "../Components/Button.svelte";
+    import Button from '../Components/Button.svelte';
+    import NavLink from "@/Components/NavLink.svelte";
+    import Value from "@/Components/Value.svelte";
 </script>
 
 <!-- Permet de modifier l'head de la page -->
@@ -8,51 +10,71 @@
 </svelte:head>
 
 <!-- Contenu de la page -->
-<main class="h-screen w-full overflow-hidden px-48 flex items-center">
-    <div class="h-[90%]">
+<main class="h-screen w-full overflow-hidden">
+    <div class="flex justify-between">
+    <div class="title">
+        <h1>DramaQuick</h1>
+    </div>
+    <nav class="">
+        <NavLink href="/">Accueil</NavLink>
+        <NavLink href="">Rejoindre une session</NavLink>
+        <NavLink href="/login">Se connecter</NavLink>
+        <NavLink href="/register">S'inscrire</NavLink>
+    </nav>
+    </div>
+    <div class="contenu">
+    <div class="left h-[90%]">
         <div class="flex flex-col gap-6 w-1/4">
             <h1 class="font-w600 text-[3rem] text-black width-606">
                 Une nouvelle version de DramaQuiz
             </h1>
-            <p class="text-lg font-w400 width-606 text-[1.5rem]">
+            <p class="text-lg font-w400 width-606 text-[1.5rem] lineh">
                 DramaQuick est une version de DramaQuiz accélérée ; des
                 questionnaires temporaires, des sessions de quiz... Tout cela a
                 été créé avec une idée : l’éphémérité !
             </p>
         </div>
-        <div>
+        <div class="margint flex flex-row">
+            <div class="space">
             <Button class="" on:click={() => (window.location.href = "/login")}
                 >Rejoindre une session</Button
             >
+            </div>
             <Button
                 class="outline"
                 on:click={() => (window.location.href = "/login")}
                 >Créer une session</Button
             >
         </div>
-        <div class="flex flex-row gap-6 text-center">
+        <div class="flex flex-row gap-20 text-center margint2">
             <div>
-                <h2 class="text-[#34FFAD] text-5xl font-w600 margin-r3">15</h2>
+                <Value value="15"/>
                 <p
-                    class="text-2xl text-[#273C33] leading-9 font-w400 margin-r3"
+                    class="text-2xl text-[#273C33] leading-9 font-w400 w-fit"
                 >
                     Quiz<br />réalisés
                 </p>
             </div>
             <div>
-                <h2 class="text-[#34FFAD] text-5xl font-w600 margin-r3">3</h2>
+                <Value value="3"/>
                 <p
-                    class="text-2xl text-[#273C33] leading-9 font-w400 margin-r3"
+                    class="text-2xl text-[#273C33] leading-9 font-w400 w-fit"
                 >
                     Sessions<br />actives
                 </p>
             </div>
             <div>
-                <h2 class="text-[#34FFAD] text-5xl font-w600">124</h2>
-                <p class="text-2xl text-[#273C33] leading-9 font-w400">
+                <Value value="124"/>
+                <p class="text-2xl text-[#273C33] leading-9 font-w400 w-fit">
                     Participa-<br />tions
                 </p>
             </div>
+        </div>
+    </div>
+        <div class="right">
+            <img class="arrow" src="/img/landing/arrow.svg" alt="" />
+            <img class="logo" src="/img/landing/logo.svg" alt="" />
+            <img class="louping" src="/img/landing/louping.svg" alt="" />
         </div>
     </div>
 </main>
@@ -77,7 +99,71 @@
         width: 606px;
     }
 
-    .margin-r3 {
-        margin-right: 3rem;
+    .title {
+        font-family: 'Poppins', sans-serif;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 32px;
+        line-height: 48px;
+        color: #000000;
+        margin-left: 14.25rem;
+        margin-top: 2.5625rem;
+    }
+
+    nav {
+        margin-top: 3.125rem;
+        margin-right: 12.125rem;
+    }
+
+    .left {
+        margin-top: 6.5625rem;
+        margin-left: 14.25rem;
+    }
+
+    .lineh {
+        line-height: 2.25rem;
+    }
+
+    .margint {
+        margin-top: 3rem;
+    }
+
+    .margint2 {
+        margin-top: 7rem;
+    }
+
+    .space {
+        margin-right: 2.7%;
+    }
+
+    .contenu {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .right {
+        display: flex;
+    }
+
+    .arrow {
+        width: 277.5px;
+        height: 202.26px;
+        margin-left: 104px;
+        margin-top: 340px;
+    }
+
+    .logo {
+        width: 522.1px;
+        height: 522.1px;
+        margin-top: 172px;
+        margin-left: 0;
+
+
+    }
+
+    .louping {
+        width: 68.74px;
+        height: 181.49px;
+        margin-top: 61px;
     }
 </style>
