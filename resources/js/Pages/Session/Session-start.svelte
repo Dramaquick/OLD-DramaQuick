61036498fc9154eb9641e07d9ca0abfd64771b30
Hugo.M <hugo.rog@icloud.com>
2023-02-19 22:15:27 +0100

    🚧 Added new components on Session-start

--- resources/js/Pages/Session/Session-start.svelte
+++ resources/js/Pages/Session/Session-start.svelte
@@ -1,4 +1,85 @@
 <script>
+    import Timer from "../../Components/Timer.svelte";
+    import { onMount } from "svelte";
+    import Tag from "../../Components/Tag.svelte";
+    import Member from "../../Components/Member.svelte";
+    import Button from "../../Components/Button.svelte";
+
+    let tag = {
+        emo1: "⌛",
+        emo2: "🎃",
+        emo3: "🍨",
+        text1: "officiel",
+        text2: "halloween",
+        text3: "nourriture",
+        color1: "#34FFAD",
+        color2: "#FFC634",
+        color3: "#FFA6E6",
+    }
+
+    // Décompteur en temps réel pour Timer
+    let timer = {
+        minutes: 0,
+        seconds: 30,
+    };
+    onMount(() => {
+        setInterval(() => {
+            if (timer.seconds > 0) {
+                timer.seconds--;
+            } else if (timer.minutes > 0) {
+                timer.minutes--;
+                timer.seconds = 59;
+            }
+        }, 1000);
+    });
+
+    let users = {
+        user1: {
+            pseudo: "Ewen.G",
+            role: "creator",
+            hasCustomIcon: false,
+        },
+        user2: {
+            pseudo: "Pierre-Louis.L",
+            role: "admin",
+            hasCustomIcon: false,
+        },
+        user3: {
+            pseudo: "Dorian.P",
+            role: "admin",
+            hasCustomIcon: false,
+        },
+        user4: {
+            pseudo: "Hugo.M",
+            role: "admin",
+            hasCustomIcon: false,
+        },
+        user5: {
+            pseudo: "Nathan.R",
+            role: "admin",
+            hasCustomIcon: false,
+        },
+        user6: {
+            pseudo: "Arthur.P",
+            role: "admin",
+            hasCustomIcon: false,
+        },
+        user7: {
+            pseudo: "Emilio.M",
+            role: "admin",
+            hasCustomIcon: false,
+        },
+        user8: {
+            pseudo: "Forfait.F",
+            role: "member",
+            hasCustomIcon: false,
+        },
+        user9: {
+            pseudo: "Jean-Bandonne.B",
+            role: "member",
+            hasCustomIcon: false,
+        },
+    }
 </script>
 
 <!-- Permet de modifier l'head de la page -->
@@ -17,12 +98,42 @@
             <p class="create">Créé par: Edouard</p>
             <p class="size">9/10</p>
         </div>
-        <div class="text">
+        <div class="container">
+        <div class="left">
             <h1 class="session-title">Quiz ultime de la mort qui fait pas trop vivre !</h1>
             <h2>Voici une petit quiz plutôt pipou pour relancer un peu la mode parce qu’en vrai c’était vachement cool ✨</h2>
         </div>
+        <div class="right">
+            <div class="left-member">
+                <Member bind:user={users.user1}/>
+                <Member bind:user={users.user2}/>
+                <Member bind:user={users.user3}/>
+                <Member bind:user={users.user4}/>
+                <Member bind:user={users.user5}/>
+            </div>
+            <div class="right-member">
+                <Member bind:user={users.user6}/>
+                <Member bind:user={users.user7}/>
+                <Member bind:user={users.user8}/>
+                <Member bind:user={users.user9}/>
+            </div>
+        </div>
     </div>
-    <div class="bottom">
+        <div class="bottom">
+            <div class="left-bottom">
+            <Timer
+                bind:minutes={timer.minutes}
+                bind:seconds={timer.seconds}
+            />
+            <Tag bind:text={tag.text1} bind:emoji={tag.emo1} bind:color={tag.color1}/>
+            <Tag bind:text={tag.text2} bind:emoji={tag.emo2} bind:color={tag.color2}/>
+            <Tag bind:text={tag.text3} bind:emoji={tag.emo3} bind:color={tag.color3}/>
+            </div>
+            <div class="right-bottom">
+                <Button class="outline">Quitter la session</Button>
+            </div>
+        </div>
+
     </div>
 </main>
 
@@ -47,7 +158,7 @@
 
     .content {
         width: 1463px;
-        height: 573px;
+        height: 625px;
         margin-left: 228px;
         margin-top: 71px;
         background: #FFFFFF;
@@ -56,11 +167,26 @@
     }
 
     .bottom {
-        width: 1920px;
-        height: 100px;
-        margin-top: 98px;
-        background: #D6F5FF;
-        border-radius: 20px 20px 0 0;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .right-bottom {
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        margin-right: 80px;
+        margin-top: 160px;
+    }
+
+    .left-bottom {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        flex-direction: row;
+        margin-left: 80px;
+        margin-top: 170px;
+        gap: 20px;
     }
 
     .top {
@@ -116,7 +242,7 @@
         color: #000000;
     }
 
-    .text h2 {
+    .left h2 {
         width: 577.14px;
         height: 106.19px;
         padding-left: 18.1px;
@@ -131,4 +257,36 @@
         border-left: 4.76px solid #34FFAD;
     }
 
+    .right {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin-right: 200px;
+        margin-top: 45px;
+    }
+
+    .left-member {
+        margin-right: 150px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: flex-start;
+        height: 120%;
+    }
+
+    .right-member {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 95%;
+        align-items: flex-start;
+    }
+
+
+    .container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        width: 100%;
+    }
 </style>
