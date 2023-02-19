6575cd612c792d08e8b0d6bbdbe98f20023d7fbe
Hugo.M <hugo.rog@icloud.com>
2023-02-19 20:45:31 +0100

    🚧 Added Timer

---
+++ resources/js/Pages/Session/Session-text.svelte
@@ -0,0 +1,36 @@
+<script>
+    import Timer from "../../Components/Timer.svelte";
+    import { onMount } from "svelte";
+
+    // Décompteur en temps réel pour Timer
+    let timer = {
+        minutes: 10,
+        seconds: 0,
+    };
+    onMount(() => {
+        setInterval(() => {
+            if (timer.seconds > 0) {
+                timer.seconds--;
+            } else if (timer.minutes > 0) {
+                timer.minutes--;
+                timer.seconds = 59;
+            }
+        }, 1000);
+    });
+</script>
+
+<main>
+    <Timer
+            bind:minutes={timer.minutes}
+            bind:seconds={timer.seconds}
+        />
+</main>
+
+<style>
+    main {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+</style>
