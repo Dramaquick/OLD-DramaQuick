6d0ab0c8b6ac751178486b7bcedb5de9c34f60c7
Hugo.M <hugo.rog@icloud.com>
2023-02-19 22:14:54 +0100

    ➡️ Added Timer component

--- resources/js/Components/Timer.svelte
+++ resources/js/Components/Timer.svelte
@@ -7,19 +7,21 @@
 </script>
 
 <span class="timer">
-    {minutes}:{secondString}
+    <p>{minutes}:</p>
+    <p>{secondString}</p>
 </span>
 
 <style>
     .timer {
         display: flex;
         align-items: center;
-        justify-content: center;
+        justify-content: start;
         font-family: 'Poppins';
         font-style: normal;
         font-weight: 600;
-        font-size: 36px;
-        line-height: 54px;
+        font-size: 2.25rem;
+        line-height: 3.375rem;
+        width: 75px;
 
         color: #000000;
     }
