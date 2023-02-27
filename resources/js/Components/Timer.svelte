f4de0d5aece563e35a46a92e412a9ef815b3fb25
Hugo.M <hugo.rog@icloud.com>
2023-02-27 09:53:12 +0100

    🐛 Fix "restantes" text beside the Timer component

--- resources/js/Components/Timer.svelte
+++ resources/js/Components/Timer.svelte
@@ -6,6 +6,9 @@
     $: secondString = seconds.toString().padStart(2, '0');
 </script>
 
-<span class="flex items-center justify-start font-semibold text-[2.25rem] text-center text-black w-20">
-{minutes}:{secondString}
+<div class="flex gap-4 items-end">
+<span class="flex items-end justify-start font-semibold text-[2.25rem] text-center text-black w-20">
+{minutes}:{secondString} 
 </span>
+<p class="text-[1.5rem] text-[#666666] font-normal w-fit h-12 flex items-center">restantes</p>
+</div>
