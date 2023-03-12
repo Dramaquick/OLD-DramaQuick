56b2f63e27108f757b7e7bd8840c4f1385f4699f
Hugo.M <hugo.rog@icloud.com>
2023-03-12 18:59:32 +0100

    🐛 Fix min-width of Tag Component

--- resources/js/Components/Tag.svelte
+++ resources/js/Components/Tag.svelte
@@ -1,18 +1,21 @@
 <script>
     import {lightenColor} from "@/Components/util/ColorUtil";
 
-    export let text="none";
-    export let emoji="❌";
-    export let color="#34FFAD";
+    export let tag = {
+        text: "none",
+        emoji: "❌",
+        color: "#34FFAD",
+    };
 </script>
 
-<span class="tag flex items-center justify-center font-semibold text-[1rem] w-fit h-fit py-[.25rem] px-4 rounded-62px text-black transition-[.2s] relative z-1" style="--tag-color: {color}; --bg-color: {lightenColor(color, -25)}">
-{emoji} {text.toUpperCase()}
+<span class="tag flex flex-row items-center justify-center font-semibold text-[1rem] w-fit h-fit py-[.25rem] px-4 rounded-62px text-black transition-[.2s] relative z-1" style="--tag-color: {tag.color}; --bg-color: {lightenColor(tag.color, -25)}">
+{tag.emoji} {tag.text.toUpperCase()}
 </span>
 
 <style>
     .tag {
         background-color: var(--tag-color);
+        min-width: fit-content;
     }
 
     .tag:hover {
