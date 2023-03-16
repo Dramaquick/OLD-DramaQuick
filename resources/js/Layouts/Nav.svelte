f0d1856c0f48d67df15a393a3442615553105849
Hugo.M <hugo.rog@icloud.com>
2023-03-16 15:02:15 +0100

    ➡️ Added modal for join a session on Nav Layout

--- resources/js/Layouts/Nav.svelte
+++ resources/js/Layouts/Nav.svelte
@@ -2,6 +2,26 @@
     import NavLink from "@/Components/NavLink.svelte";
     import UserButton from "@/Components/UserButton.svelte";
     import { page } from '@inertiajs/inertia-svelte';
+    import Notification from "@/Components/Notification.svelte";
+
+    function notify(title, text, type, duration, format, position, input, placeholder) {
+        const notification = document.createElement('div');
+        document.body.appendChild(notification);
+
+        new Notification({
+            target: notification,
+            props: {
+                title,
+                text,
+                type,
+                duration,
+                format,
+                position,
+                input,
+                placeholder
+            }
+        });
+    }
 </script>
 
 <main class="h-screen w-full overflow-hidden bg-cover bg-no-repeat">
@@ -9,9 +29,11 @@
         <h1 class="font-semibold text-[2rem] text-black">DramaQuick</h1>
         <div class="content flex flex-row gap-2">
             <NavLink href="/">Accueil</NavLink>
-            <NavLink href="">Rejoindre une session</NavLink>
+            <NavLink href="" action={() => {notify("Rejoindre une session","","normal",0,"box","middle",true,"Entrer le code de session")}}>Rejoindre une session</NavLink>
             {#if $page.props.auth.user}
+            <div class="pl-8">
                 <UserButton user={$page.props.auth.user} />
+            </div>
             {:else}
                 <NavLink href="/login">Se connecter</NavLink>
                 <NavLink href="/register">S'inscrire</NavLink>
