bb5b23d7d080d65fd27bf30851706f74f1a5cac9
Hugo.M <hugo.rog@icloud.com>
2023-03-17 14:42:18 +0100

    🐛 Fix color on Pie Component

--- resources/js/Components/PieChart.svelte
+++ resources/js/Components/PieChart.svelte
@@ -3,39 +3,48 @@
     import "@carbon/charts/styles.css";
     import { PieChart } from "@carbon/charts-svelte";
 
+    let colors = [
+      "#f0e583",
+      "#dbe888",
+      "#c6eb8c",
+      "#b1ee91",
+      "#9cf196",
+      "#88f39b",
+      "#73f69f",
+      "#5ef9a4",
+      "#49fca8",
+      "#34ffad",
+    ]
+
     export let data = [
       {
         "group": "2V2N 9KYPM version 1",
-        "count": 28000
+        "value": 28000
       },
       {
         "group": "L22I P66EP L22I P66EP L22I P66EP",
-        "count": 65000
+        "value": 65000
       },
       {
         "group": "JQAI 2M4L1",
-        "count": 75000
+        "value": 75000
       },
       {
         "group": "J9DZ F37AP",
-        "count": 3200
+        "value": 3200
       },
       {
         "group": "YEL48 Q6XK YEL48",
-        "count": 15000
+        "value": 15000
       },
       {
         "group": "Misc",
-        "count": 25000
+        "value": 25000
       }
     ];
 
     export let options = {
         resizable: true,
-        legend: {
-            enabled: true,
-            position: "bottom",
-        },
         "pie": {
         "alignment": "center"
         },
@@ -45,7 +54,56 @@
         toolbar: {
             enabled: false,
         },
+        legend: {
+            enabled: false,
+        },
+        tooltip: {
+            enabled: false,
+        },
+        color: {
+            scale: {},
+        },
     };
+
+    let amoutOfCountDifferently = [];
+
+    data.forEach((item, index) => {
+        if (!amoutOfCountDifferently.includes(item.value)) {
+            amoutOfCountDifferently.push(item.value);
+        }
+    });
+
+    //tri de manière décroissante amoutOfCountDifferently
+    amoutOfCountDifferently.sort((a, b) => {
+        return b - a;
+    });
+
+    let colorIndex = 0;
+    let CountColor = {};
+
+    amoutOfCountDifferently.forEach((item, index) => {
+        CountColor[item] = colors[Math.round(colorIndex/amoutOfCountDifferently.length*10)];
+        colorIndex++;
+    });
+
+    data.forEach((item, index) => {
+        options.color.scale[item.group] = CountColor[item.value];
+    });
+
+    //On trie les datas en fonction de leur value dans l'ordre croissant
+    data.sort((a, b) => {
+        return b.value - a.value;
+    });
 </script>
 
+<div class="flex flex-row items-center justify-center">
 <PieChart {data} {options} />
+<div class="flex flex-col gap-6">
+{#each data as item}
+    <div class="flex flex-row items-center">
+        <div class="w-4 h-4 rounded-full" style="background-color: {options.color.scale[item.group]}"></div>
+        <div class="ml-2">{item.group}, nombre de votes: {item.value}</div>
+    </div>
+{/each}
+</div>
+</div>
