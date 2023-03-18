f0a1ef72c78a9ebb7be3b14a3ff154fbc315381a
Proxyfil <piloupilou200@gmail.com>
2023-03-18 16:08:54 +0100

    🚚A dded first routes to update user infos

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChangeUsernameController;
+use App\Http\Controllers\ChangeEmailController;
+use App\Http\Controllers\ChangeAvatarController;
+
+'links' => [
+    public_path('storage') => storage_path('app/public'),
+    public_path('avatars') => storage_path('app/avatars'),
+]
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +25,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/user/change-username', ChangeUsernameController::class)
+    ->middleware('auth')
+    ->missing(function (Request $request) {
+        return Redirect::route('/change-username.index');
+    });;
+
+Route::resource('/user/change-email', ChangeEmailController::class)
+    ->middleware('auth')
+    ->missing(function (Request $request) {
+        return Redirect::route('/user/change-email.index');
+    });;
+
+Route::resource('/user/change-avatar', ChangeAvatarController::class)
+    ->middleware('auth')
+    ->missing(function (Request $request) {
+        return Redirect::route('/user/change-avatar.index');
+    });;
